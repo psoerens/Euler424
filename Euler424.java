f2cc35ae3033bc58b6ebdca2d163162592b3f3ca
psoerens <p.soerens@outlook.com>
2019-04-26 12:52:59 -0700

    Inital setup

---
+++ Euler424.java
@@ -0,0 +1,15 @@
+/**
+ * Project Euler 424: Kakuro
+ * Find the sum of the answers to 200 kakuro puzzles
+ * details found on: https://projecteuler.net/problem=424
+ * @author Phil
+ *
+ */
+public class Euler424 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

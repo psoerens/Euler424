47325f03bb174ab47b57e14c1128a01704b9243b
p <psoerens@users.noreply.github.com>
2019-04-26 15:33:15 -0700

    open and close function

--- EulerParser.java
+++ EulerParser.java
@@ -4,7 +4,66 @@
  *
  */
 
+import java.io.*; //buffered reader
+import java.util.Arrays; //potential use for array manipulation
+
 
 public class EulerParser {
 
+	private File f;	//file to be read from
+	private BufferedReader reader; //reader
+	
+	
+	/*
+	 * interface methods
+	 */
+	public void fileOpen(File newFile){
+		
+		open(newFile);
+	}
+	
+	public void closeFile(){
+		
+		close();
+	}
+	
+	public void readFile(){
+		
+		read();
+	}
+	
+	/*
+	 * action methods
+	 */
+	
+	//open the reader
+	private void open(File newFile)	{
+		
+		reader = null; //null initialize
+		f = newFile;
+		
+		try{
+			
+			reader = new BufferedReader(new FileReader(f));
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private void read(){
+		
+	}
+	
+	//close the reader
+	private void close(){
+		
+		try{
+			
+			reader.close();
+		} catch (Exception e){
+			
+			e.printStackTrace();
+		}
+	}
 }
